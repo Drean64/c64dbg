09aee480ff9d9ff771d45146a9ad63a92bc0d105
Ernesto Borio <ernestoborio@gmail.com>
2021-09-20 00:35:23 -0300

    Beginning web interface and emulation inside go routine.

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/Drean64/c64"
-	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-	c := c64.Make(c64.NTSC)
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
-	}
+
+	com64 := c64.Make(c64.NTSC)
+	events := make(chan interface{})
+	commands := make(chan interface{})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/index.html")
+	})
+
+	go com64.Run(events, commands)
+	go func() {
+		time.Sleep(1 * time.Second)
+		close(commands)
+	}()
+
+	port := ":6464"
+	fmt.Printf("go64 debugger listening on http://localhost%s\n", port)
+	http.ListenAndServe(port, nil)
 }
